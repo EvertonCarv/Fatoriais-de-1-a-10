2af685d4a497be966def14221d2d86457180f7b2
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2021-12-31 11:55:38 -0300

    Adicionar arquivos de projeto.

---
+++ Fatoriais de 1 a 10/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Fatoriais de 1 a 10");
+
+
+int fatorial = 1;
+
+for (int i = 1; i < 11; i++)
+{
+    fatorial *= i;
+    Console.WriteLine($"Fatorial de {i} = {fatorial}");
+}
